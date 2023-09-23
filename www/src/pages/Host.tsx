b195872f369711b1611bfa42c65659a4ef30cbda
Michael Stevens <mls0.5mv2@gmail.com>
2023-09-23 12:00:04 -0400

    host page shows users

--- www/src/pages/Host.tsx
+++ www/src/pages/Host.tsx
@@ -1,7 +1,35 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+interface IPlayer {
+  name: string;
+  score: bigint;
+}
+
+interface IState {
+  players: Array<string>;
+}
+
 const Host = () => {
+  const [state, setState] = useState<IState>({players: []});
+
+  const updateState = useCallback(async () => {
+    const response = await fetch('/api/getgame');
+    const data = await response.json();
+    const playerData: Array<IPlayer> = data?.players || [];
+    const players = playerData.map(p => p.name);
+
+    setState({players});
+  }, []);
+
+  useEffect(() => {
+    setInterval(updateState, 1000);
+  }, [updateState]);
+
+  const playerList = state.players.map((player, i) => <li key={i}>{player}</li>);
+
   return (
     <div className="App">
-      yo yo
+    {playerList}
     </div>
   );
 }
