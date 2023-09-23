c64a6cf6f575b9342ee1add97303723f1fc94009
Shawn Verma <shawn.verma@spectralink.com>
2023-09-23 00:17:05 -0400

    routing kind of

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import User from './pages/User';
+// import Host from './pages/User';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+        <Routes>
+           <Route path="/" element={<User />} />
+           {/* <Route path="/host" element={<Host />} /> */}
+        </Routes>
+     </>
   );
-}
+ };
 
 export default App;
--- www/src/index.tsx
+++ www/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import User from './pages/User';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <User />
   </React.StrictMode>
 );
